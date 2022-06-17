1985ea72db0cbc3e5d66a6ba10bf9e04154e5e9b
alvaroormeno <alvaro@ormeno.org>
2022-06-17 17:17:12 -0400

    basic html and tailwindcss installed and running

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2 className="text-3xl font-bold underline">Become a Host</h2>
+      <div>Host your space, share your world</div>
+      <p>
+        The art of hosting is rooted in the thoughtful design. 
+        Share your unique aesthetic with guests and extra income on a schedule that works for you.
+      </p>
+      <button>Try hosting</button>
+      <h2>Your next chapter, made possible by Hosting</h2>
+      <h3>Share what you love</h3>
+      <p>
+        Indulge your love for design when you host Airbnb guests, who are drawn to authentic travel in creativelu curated
+      </p>
+      <h3>Host how you want to</h3>
+      <p>Airbnb gives your the tools and support to earn extra income, be your own boss and invest in your passions</p>
+
     </div>
+
   );
 }
 
