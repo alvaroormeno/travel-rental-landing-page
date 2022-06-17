31363ff8707202c7eb129978e5901ce612ee4222
alvaroormeno <alvaro@ormeno.org>
2022-06-17 18:44:24 -0400

    both section 1 and 2 90% ready, section 1 missing gradient for image, section 2 missing small details

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-
+import houseImg from '../src/assets/travel-house.jpg'
 
 function App() {
+
+  const bgImageStyle = {
+    backgroundImage: `url(${houseImg})`,
+    backgroundSize: 'cover'
+  }
+
+
   return (
     <div>
-      {/* SECTION 1 */}
-      <div className="bg-slate-700 text-white px-8 py-16">
-        {/* CONTAINER for text and button */}
-        <div className="border-2 border-green-300 max-w-xl grid grid-cols-1 gap-8">
+      
+      {/* PAGE SECTION 1 */}
+      <div className="text-white px-8 py-16" style={bgImageStyle}>
+        {/* MAIN CONTAINER */}
+        <div className=" border-2 border-green-300 max-w-xl grid grid-cols-1 gap-8">
           <h2 className="text-xl uppercase font-bold">Become a Host</h2>
           <div className="text-6xl font-bold">Host your space, share your world</div>
           <p className="text-lg">
@@ -16,15 +24,42 @@ function App() {
           <button className="bg-purple-500 py-3 px-6 text-lg rounded-md w-48">Try hosting</button>
         </div> 
       </div>
+
+      
+      
+      {/* PAGE SECTION 2 */}
+      <div className=" px-8 py-16">
+
+        {/* CONTAINER for big main text */}
+        <div className=" border-2 border-green-300 max-w-sm mb-16">
+          <h2 className="text-5xl">Your next chapter, made possible by Hosting</h2>
+        </div>
+
+        {/* CONTAINER for 2 columns */}
+        <div className="border-2 border-green-300 grid grid-cols-2 gap-4 text-slate-600">
+          {/* COLUMN 1 */}
+          <div className="border-2 border-blue-300">
+            <h3 className="text-2xl font-bold mb-2">Share what you love</h3>
+            <p className="text-lg">
+              Indulge your love for design when you host Airbnb guests, who are drawn to authentic travel in creativelu curated
+            </p>
+          </div>
+          {/* COLUMN 2 */}
+          <div className="border-2 border-blue-300">
+            <h3 className="text-2xl font-bold mb-2">Host how you want to</h3> 
+            <p>
+              Airbnb gives your the tools and support to earn extra income, be your own boss and invest in your passions
+            </p>
+          </div>
+          
+        </div>
+        
+        
+      </div>
+      
       
         
-      <h2>Your next chapter, made possible by Hosting</h2>
-      <h3>Share what you love</h3>
-      <p>
-        Indulge your love for design when you host Airbnb guests, who are drawn to authentic travel in creativelu curated
-      </p>
-      <h3>Host how you want to</h3> 
-      <p>Airbnb gives your the tools and support to earn extra income, be your own boss and invest in your passions</p>
+      
 
     </div>
 
