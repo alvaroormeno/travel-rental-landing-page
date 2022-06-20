3ded152432f684d6c22d370ba45f44f2e8de2b62
alvaroormeno <alvaro@ormeno.org>
2022-06-20 17:22:04 -0400

    completed

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         {/* GRADIENT BACKGROUND */}
         <div className='bg-gradient-to-r from-black'>
           {/* MAIN CONTAINER */}
-          <div className="px-8 py-16 border-2 border-green-300 max-w-xl grid grid-cols-1 gap-8">
+          <div className="px-8 py-16 max-w-xl grid grid-cols-1 gap-8">
             <div className='w-12'>
               <ReactLogo className='fill-white '></ReactLogo>
             </div>
@@ -41,21 +41,21 @@ function App() {
       <div className=" px-8 py-16">
 
         {/* CONTAINER for big main text */}
-        <div className=" border-2 border-green-300 max-w-md mb-16">
+        <div className=" max-w-md mb-16">
           <h2 className="text-5xl">Your next chapter, made possible by Hosting</h2>
         </div>
 
         {/* CONTAINER for 2 columns */}
-        <div className="border-2 border-green-300 grid grid-cols-2 gap-4 text-slate-600">
+        <div className=" grid grid-cols-2 gap-4 text-slate-600">
           {/* COLUMN 1 */}
-          <div className="border-2 border-blue-300">
+          <div className="">
             <h3 className="text-2xl font-bold mb-2">Share what you love</h3>
             <p className="text-lg">
               Indulge your love for design when you host Airbnb guests, who are drawn to authentic travel in creativelu curated
             </p>
           </div>
           {/* COLUMN 2 */}
-          <div className="border-2 border-blue-300">
+          <div className="">
             <h3 className="text-2xl font-bold mb-2">Host how you want to</h3> 
             <p>
               Airbnb gives your the tools and support to earn extra income, be your own boss and invest in your passions
